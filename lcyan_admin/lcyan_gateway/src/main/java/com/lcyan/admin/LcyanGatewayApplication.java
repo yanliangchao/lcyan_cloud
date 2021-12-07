ab83310ba04cb7a06f4ee52a95e3c8073467fb63
18196646113@163.com <yy535930>
2021-12-07 21:08:47 +0800

    frist

---
+++ lcyan_admin/lcyan_gateway/src/main/java/com/lcyan/admin/LcyanGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lcyan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Alex
+ * @Date: 2021-12-07 20:45
+ */
+@SpringBootApplication
+public class LcyanGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcyanGatewayApplication.class, args);
+    }
+}
